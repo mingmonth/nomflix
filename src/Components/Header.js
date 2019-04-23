ce1aacaa8bca9c9f4b8fdb1efe489c7f6b1516c1
mingmonth <ys9922@naver.com>
2019-04-23 23:11:34 +0900

    #3.2 CSS in React part Three

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,17 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Header = styled.header``;
+
+const List = styled.ul`
+  display: flex;
+  &:hover {
+    background-color: blue;
+  }
+`;
+
+const Item = styled.li``;
+
+const SLink = styled(Link)``;
 
 export default () => (
-  <header>
-    <ul>
-      <li>
-        <a href="/">Movies</a>
-      </li>
-      <li>
-        <a href="/tv">TV</a>
-      </li>
-      <li>
-        <a href="/search">Search</a>
-      </li>
-    </ul>
-  </header>
+  <Header>
+    <List>
+      <Item>
+        <SLink href="/">Movies</SLink>
+      </Item>
+      <Item>
+        <SLink href="/tv">TV</SLink>
+      </Item>
+      <Item>
+        <SLink href="/search">Search</SLink>
+      </Item>
+    </List>
+  </Header>
 );
