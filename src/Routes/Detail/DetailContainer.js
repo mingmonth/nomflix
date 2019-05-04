ee35aee7ee7b284d1afe48fc65ddf391065b788f
mingmonth <ys9922@naver.com>
2019-05-04 22:13:57 +0900

    #5.6 Detail Container part Two

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -1,12 +1,20 @@
 import React from "react";
 import DetailPresenter from "./DetailPresenter";
+import { moviesApi, tvApi } from "api";
 
 export default class extends React.Component {
-  state = {
-    result: null,
-    error: null,
-    loading: true
-  };
+  constructor(props) {
+    super(props);
+    const {
+      location: { pathname }
+    } = props;
+    this.state = {
+      result: null,
+      error: null,
+      loading: true,
+      isMovie: pathname.includes("/movie/")
+    };
+  }
 
   async componentDidMount() {
     const {
@@ -15,16 +23,39 @@ export default class extends React.Component {
       },
       history: { push }
     } = this.props;
-    //console.log(parseInt(id));
+
+    const { isMovie } = this.state;
+    // console.log(this.props);
+    // console.log(parseInt(id));
     const parseId = parseInt(id);
     if (isNaN(parseId)) {
       return push("/");
     }
+    // console.log(this.isMovie);
+    let result = null;
+    try {
+      /*if (isMovie) {
+        const request = await moviesApi.detail(parseId);
+        result = request.data;
+      } else {
+        const request = await tvApi.detail(parseId);
+        result = request.data;
+      }*/
+      const { data: result } = isMovie
+        ? await moviesApi.detail(id)
+        : await tvApi.detail(id);
+      console.log(result);
+    } catch {
+      this.setState({ error: "Can't find anything." });
+    } finally {
+      this.setState({ loading: false, result });
+    }
   }
 
   render() {
-    console.log(this.props);
+    // console.log(this.props);
     const { result, error, loading } = this.state;
+    console.log(this.state);
     return <DetailPresenter result={result} error={error} loading={loading} />;
   }
 }
